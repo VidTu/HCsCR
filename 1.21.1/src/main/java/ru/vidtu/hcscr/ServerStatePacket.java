ed60571c0ea09c239f3a3b008d0e11e032a0957d
VidTu <imvidtu@proton.me>
2025-01-06 06:44:25 +0300

    chore(copyright): 2025

--- 1.21.1/src/main/java/ru/vidtu/hcscr/ServerStatePacket.java
+++ 1.21.1/src/main/java/ru/vidtu/hcscr/ServerStatePacket.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2023 Offenderify
- * Copyright (c) 2023-2024 VidTu
+ * Copyright (c) 2023-2025 VidTu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
