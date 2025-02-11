592fe13dcdbdd167a5a982d9987a639961699a40
VidTu <imvidtu@proton.me>
2025-02-11 13:02:40 +0300

    build: Enable renovate config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,9 +20,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
         maven("https://maven.architectury.dev/")
-        maven("https://maven.quiltmc.org/repository/release/")
         maven("https://maven.minecraftforge.net/")
-        maven("https://maven.neoforged.net/releases/")
     }
 }
 
@@ -31,7 +29,4 @@ plugins {
 }
 
 rootProject.name = "HCsCR"
-val versions = listOf("1.16.5", "1.17.1", "1.18.2", "1.19.2", "1.19.4", "1.20.1", "1.20.2", "1.20.4", "1.20.6", "1.21.1", "1.21.3", "1.21.4", "1.21.5")
-for (version in versions) {
-    include(version)
-}
+include("1.16.5", "1.17.1", "1.18.2", "1.19.2", "1.19.4", "1.20.1", "1.20.2", "1.20.4", "1.20.6", "1.21.1", "1.21.3", "1.21.4", "1.21.5")
