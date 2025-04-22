df8736b6daca21201e6b91022381de0f1845050b
VidTu <imvidtu@proton.me>
2025-04-22 16:35:48 +0300

    build: Build on actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -213,6 +213,7 @@ tasks.withType<Jar> {
 }
 
 tasks.withType<RemapJarTask> {
+    destinationDirectory = rootProject.layout.buildDirectory.asFile
     val minifier = UnsafeUnaryOperator<String> { Gson().fromJson(it, JsonElement::class.java).toString() }
     doLast {
         ZipUtils.transformString(archiveFile.get().asFile.toPath(), mapOf(
