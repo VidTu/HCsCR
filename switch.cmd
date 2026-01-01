e15243611b0b4c38c0b956b748034dd838c3a757
VidTu <imvidtu@proton.me>
2026-01-01 08:25:18 +0300

    chore(copyright): Happy new year (2026)

--- switch.cmd
+++ switch.cmd
@@ -2,7 +2,7 @@
 :: that allows removing the end crystals faster.
 ::
 :: Copyright (c) 2023 Offenderify
-:: Copyright (c) 2023-2025 VidTu
+:: Copyright (c) 2023-2026 VidTu
 ::
 :: Licensed under the Apache License, Version 2.0 (the "License");
 :: you may not use this file except in compliance with the License.
